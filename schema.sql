6662a470c59c210e23a2280ece9b3fed9c32f585
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-08-02 13:51:37 +0100

    Add performance audit solution to project

--- schema.sql
+++ schema.sql
@@ -63,3 +63,23 @@ CREATE TABLE specializations (
     FOREIGN KEY(animals_id) REFERENCES animals(id) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY(vets_id) REFERENCES vets(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+  -- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- Removed NOT NULL in age of owners TABLE
+ALTER TABLE owners
+ALTER COLUMN age
+DROP NOT NULL;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- perfomance audit
+CREATE INDEX animals_id ON visits (animal_id);
+CREATE INDEX vets_id ON visits (vet_id);
+CREATE INDEX index_email ON owners (email);
